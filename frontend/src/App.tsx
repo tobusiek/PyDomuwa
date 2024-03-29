b5eab5ddd99731b59ab6dbe5235000024ced4771
tobasqo <s.jasiek.gaming@gmail.com>
2024-03-29 22:56:01 +0100

    make first request to api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios, { AxiosResponse } from "axios";
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse = await axios.get(
+          "http://localhost:2137/",
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "X-Session-Key": "guccitrip",
+            },
+          },
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +42,10 @@ function App() {
         >
           Learn React
         </a>
+        {data ? <p>API Ping: {data}</p> : <p>no data</p>}
       </header>
     </div>
   );
-}
+};
 
 export default App;
