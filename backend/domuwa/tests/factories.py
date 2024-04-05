d755b8ea708711524ce004f710a5259ba492663e
tobasqo <s.jasiek.gaming@gmail.com>
2024-04-05 23:43:48 +0200

    add GameType factory

--- backend/domuwa/tests/factories.py
+++ backend/domuwa/tests/factories.py
@@ -4,33 +4,12 @@ from factory.alchemy import SQLAlchemyModelFactory
 from domuwa.models import db_models
 
 
-class QuestionFactory(SQLAlchemyModelFactory):
-    game_name = "ego"
-    category = "SFW"
-    author = factory.Sequence(lambda x: "Author %d" % x)
-    text = "question's text"
-    excluded = False
+class GameTypeFactory(SQLAlchemyModelFactory):
+    name = db_models.GameTypeChoices.EGO
 
     class Meta:  # type: ignore
-        model = db_models.Question
-
-
-class AnswerFactory(SQLAlchemyModelFactory):
-    author = factory.Sequence(lambda x: "Author %d" % x)
-    text = "answer's text"
-    correct = False
-    question: factory.SubFactory = factory.SubFactory(QuestionFactory)
-
-    class Meta:  # type: ignore
-        model = db_models.Answer
-
-
-class GameRoomFactory(SQLAlchemyModelFactory):
-    game_name = "ego"
-    game_category = "SFW"
-
-    class Meta:  # type: ignore
-        model = db_models.GameRoom
+        model = db_models.GameType
+        sqlalchemy_session_persistence = "commit"
 
 
 class PlayerFactory(SQLAlchemyModelFactory):
@@ -38,12 +17,4 @@ class PlayerFactory(SQLAlchemyModelFactory):
 
     class Meta:  # type: ignore
         model = db_models.Player
-        sqlalchemy_session_persistence = "commit"
-
-
-class GameTypeFactory(SQLAlchemyModelFactory):
-    name = factory.Sequence(lambda x: "GameType %d" % x)
-
-    class Meta:  # type: ignore
-        model = db_models.GameType
         sqlalchemy_session_persistence = "commit"
