e5c5a0c74c4d3dccda48e8c42ca6d583e037163e
tobasqo <szymon.j.jaskowski@gmail.com>
2024-07-02 22:41:00 +0200

    base for question services

--- backend/domuwa/services/questions_services.py
+++ backend/domuwa/services/questions_services.py
@@ -5,7 +5,34 @@ from domuwa import database as db
 from domuwa.models.question import Question
 
 
+async def create_question(
+    question: Question,
+    db_sess: Session = Depends(db.get_db_session),
+):
+    raise NotImplementedError()
+
+
 async def get_question_by_id(
-    question_id: int, db_sess: Session = Depends(db.get_db_session)
+    question_id: int,
+    db_sess: Session = Depends(db.get_db_session),
 ):
     return await db.get(question_id, Question, db_sess)
+
+
+async def get_all_questions(db_sess: Session = Depends(db.get_db_session)):
+    raise NotImplementedError()
+
+
+async def update_question(
+    question_id: int,
+    question: Question,
+    db_sess: Session = Depends(db.get_db_session),
+):
+    raise NotImplementedError()
+
+
+async def delete_question(
+    question_id: int,
+    db_sess: Session = Depends(db.get_db_session),
+):
+    raise NotImplementedError()
