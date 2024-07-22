92faf40e18c7ca4ee247e9df92022bc11b70d08c
tobasqo <szymon.j.jaskowski@gmail.com>
2024-07-23 00:05:56 +0200

    rewrite answers router using introduced routers

--- backend/domuwa/routers/answers_router.py
+++ backend/domuwa/routers/answers_router.py
@@ -1,72 +1,78 @@
 import logging
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import ValidationError
+from fastapi import APIRouter, Depends, status
+from fastapi_class import Method, View, endpoint
 from sqlmodel import Session
 
 from domuwa.database import get_db_session
 from domuwa.models.answer import (
     Answer,
     AnswerCreate,
+    AnswerRead,
     AnswerUpdate,
-    AnswerWithQuestionRead,
 )
-from domuwa.services import answers_services as services
-
-logger = logging.getLogger(__name__)
+from domuwa.routers.generics import (
+    CreateRouter,
+    DeleteRouter,
+    RetrieveRouter,
+    UpdateRouter,
+)
+from domuwa.services.answers_services import AnswerServices
 
 router = APIRouter(prefix="/answers", tags=["Answer"])
 
 
-@router.post(
-    "/",
-    response_model=AnswerWithQuestionRead,
-    status_code=status.HTTP_201_CREATED,
-)
-async def create_answer(
-    answer_create: AnswerCreate,
-    db_sess: Session = Depends(get_db_session),
+@View(router)
+class AnswerRouter(
+    CreateRouter[AnswerCreate, Answer],
+    RetrieveRouter[Answer],
+    UpdateRouter[AnswerUpdate, Answer],
+    DeleteRouter[Answer],
 ):
-    logger.debug("received Answer(%s) to create", answer_create)
-    try:
-        answer = Answer.model_validate(answer_create, strict=True)
-    except ValidationError as exc:
-        logger.error(str(exc))
-        raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY, str(exc)) from exc
+    services = AnswerServices()
+    logger = logging.getLogger(__name__)
+    db_model_type_name = Answer.__name__
 
-    return await services.create_answer(answer, db_sess)
+    RESPONSE_MODEL_ATTRIBUTE_NAME = {
+        "create_answer": AnswerRead,
+        "get_answer_by_id": AnswerRead,
+        "get_all_answers": list[AnswerRead],
+        "update_answer": AnswerRead,
+    }
 
+    @endpoint(Method.POST, status_code=status.HTTP_201_CREATED)
+    async def create(
+        self,
+        answer_create: AnswerCreate,
+        session: Session = Depends(get_db_session),
+    ):
+        return await self._post(answer_create, session)
 
-@router.get("/{answer_id}", response_model=AnswerWithQuestionRead)
-async def get_answer_by_id(answer_id: int, db_sess: Session = Depends(get_db_session)):
-    logger.debug("received Answer(id=%d) to get", answer_id)
-    return await services.get_answer_by_id(answer_id, db_sess)
+    @endpoint(Method.GET, path="{answer_id}")
+    async def get_by_id(
+        self,
+        answer_id: int,
+        session: Session = Depends(get_db_session),
+    ):
+        return await self._get_by_id(answer_id, session)
 
+    @endpoint(Method.GET)
+    async def get_all(self, session: Session = Depends(get_db_session)):
+        return await self._get_all(session)
 
-@router.get("/", response_model=list[AnswerWithQuestionRead])
-async def get_all_answers(db_sess: Session = Depends(get_db_session)):
-    return await services.get_all_answers(db_sess)
+    @endpoint(Method.PATCH, path="{answer_id}")
+    async def update(
+        self,
+        answer_id: int,
+        answer_update: AnswerUpdate,
+        session: Session = Depends(get_db_session),
+    ):
+        return await self._patch(answer_id, answer_update, session)
 
-
-@router.patch("/{answer_id}", response_model=AnswerWithQuestionRead)
-async def update_answer(
-    answer_id: int,
-    answer_update: AnswerUpdate,
-    db_sess: Session = Depends(get_db_session),
-):
-    logger.debug(
-        "received Answer(%s) to update Answer(id=%d)", answer_update, answer_id
-    )
-    try:
-        answer = Answer.model_validate(answer_update, strict=True)
-    except ValidationError as exc:
-        logger.error(str(exc))
-        raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY, str(exc)) from exc
-
-    return await services.update_answer(answer_id, answer, db_sess)
-
-
-@router.delete("/{answer_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_answer(answer_id: int, db_sess: Session = Depends(get_db_session)):
-    logger.debug("received Answer(id=%d) to remove", answer_id)
-    await services.delete_answer(answer_id, db_sess)
+    @endpoint(Method.DELETE, path="{answer_id}", status_code=status.HTTP_204_NO_CONTENT)
+    async def delete(
+        self,
+        answer_id: int,
+        session: Session = Depends(get_db_session),
+    ):
+        return await self._delete(answer_id, session)
