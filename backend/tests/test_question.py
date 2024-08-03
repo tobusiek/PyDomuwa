dabec79e58f6076713964ff11523c29594852262
tobasqo <szymon.j.jaskowski@gmail.com>
2024-08-04 00:15:38 +0200

    fix question tests, use common test case

--- backend/tests/test_question.py
+++ backend/tests/test_question.py
@@ -1,167 +1,112 @@
-from typing import Any
-
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+from sqlmodel import Session
 
 from domuwa.models.question import Question
+from domuwa.services.questions_services import QuestionServices
+from tests.common_tc import CommonTestCase
 from tests.factories import (
+    AnswerFactory,
     GameTypeFactory,
     PlayerFactory,
     QnACategoryFactory,
     QuestionFactory,
 )
 
-QUESTIONS_PREFIX = "/api/questions/"
 
+class TestQuestion(CommonTestCase[Question]):
+    path = "/api/questions/"
+    services = QuestionServices()
 
-def dump_question(question: Question):
-    return question.model_dump()
+    def assert_valid_response(self, response_data: dict) -> None:
+        assert "id" in response_data, response_data
+        assert "text" in response_data, response_data
+        assert "excluded" in response_data, response_data
+        assert "author" in response_data, response_data
+        assert "game_type" in response_data, response_data
+        assert "game_category" in response_data, response_data
 
+    def assert_valid_response_values(self, response_data: dict, model: Question) -> None:
+        assert response_data["id"] == model.id
+        assert response_data["text"] == model.text
+        assert response_data["excluded"] == model.excluded
+        assert response_data["author"]["id"] == model.author.id  # type: ignore
+        assert response_data["game_type"]["id"] == model.game_type.id  # type: ignore
+        assert response_data["game_category"]["id"] == model.game_category.id  # type: ignore
 
-def assert_valid_response(response_data: dict[str, Any]):
-    assert "id" in response_data, response_data
-    assert "text" in response_data, response_data
-    assert "excluded" in response_data, response_data
-    assert "author" in response_data, response_data
-    assert "game_type" in response_data, response_data
-    assert "game_category" in response_data, response_data
+    async def assert_valid_delete(self, model_id: int, db_session: Session) -> None:
+        question = await self.services.get_by_id(model_id, db_session)
+        assert question is not None
+        assert question.deleted
+        for answer in question.answers:
+            assert answer.deleted
 
+    def build_model(self) -> Question:
+        author = PlayerFactory.create()
+        game_type = GameTypeFactory.create()
+        game_category = QnACategoryFactory.create()
+        return QuestionFactory.build(
+            author_id=author.id,
+            game_type_id=game_type.id,
+            game_category_id=game_category.id,
+        )
 
-def build_question():
-    author = PlayerFactory.create()
-    game_type = GameTypeFactory.create()
-    game_category = QnACategoryFactory.create()
-    return QuestionFactory.build(
-        author_id=author.id,
-        game_type_id=game_type.id,
-        game_category_id=game_category.id,
-    )
+    def create_model(self) -> Question:
+        author = PlayerFactory.create()
+        game_type = GameTypeFactory.create()
+        game_category = QnACategoryFactory.create()
+        return QuestionFactory.create(
+            author_id=author.id,
+            game_type_id=game_type.id,
+            game_category_id=game_category.id,
+        )
 
+    def test_update(self, api_client: TestClient):
+        question = self.create_model()
+        new_text = "new text"
 
-def create_question():
-    author = PlayerFactory.create()
-    game_type = GameTypeFactory.create()
-    game_category = QnACategoryFactory.create()
-    return QuestionFactory.create(
-        author_id=author.id,
-        game_type_id=game_type.id,
-        game_category_id=game_category.id,
-    )
+        response = api_client.patch(
+            f"{self.path}{question.id}",
+            json={"text": new_text},
+        )
+        assert response.status_code == status.HTTP_200_OK, response.text
+        response_data = response.json()
+        self.assert_valid_response(response_data)
 
+        assert response_data["id"] >= question.id, response_data
 
-def test_create_question(api_client: TestClient):
-    question = build_question()
+        assert response_data["text"] != question.text
+        assert response_data["text"] == new_text
 
-    response = api_client.post(QUESTIONS_PREFIX, json=dump_question(question))
-    assert response.status_code == status.HTTP_201_CREATED, response.text
-    response_data = response.json()
-    assert_valid_response(response_data)
+        assert response_data["excluded"] == question.excluded, response_data
 
-    response = api_client.get(f"{QUESTIONS_PREFIX}{response_data['id']}")
-    assert response.status_code == status.HTTP_200_OK, response.text
-    response_data = response.json()
-    assert_valid_response(response_data)
+        # TODO: after auth
+        # assert response_data["author"]["id"] != question.author.id
+        # assert response_data["author"]["id"] == new_author.id
 
+        assert response_data["game_type"]["id"] == question.game_type.id, response_data  # type: ignore
+        assert response_data["game_type"]["name"] == question.game_type.name  # type: ignore
 
-def test_get_question_by_id(api_client: TestClient):
-    question = create_question()
+        assert (
+                response_data["game_category"]["id"] == question.game_category.id  # type: ignore
+        ), response_data
+        assert response_data["game_category"]["name"] == question.game_category.name  # type: ignore
 
-    response = api_client.get(f"{QUESTIONS_PREFIX}{question.id}")
-    assert response.status_code == status.HTTP_200_OK, response.text
-    response_data = response.json()
-    assert_valid_response(response_data)
+        assert not response_data["answers"], response_data
 
-    assert response_data["id"] == question.id
-    assert response_data["text"] == question.text
-    assert response_data["excluded"] == question.excluded
-    assert response_data["author"]["id"] == question.author.id
-    assert response_data["game_type"]["id"] == question.game_type.id
-    assert response_data["game_category"]["id"] == question.game_category.id
+    @pytest.mark.asyncio
+    async def test_delete_with_answers(self, api_client: TestClient, db_session: Session):
+        model = self.create_model()
+        model_id = model.id
+        assert model_id is not None
 
+        AnswerFactory.create_batch(2, question_id=model_id)
 
-def test_get_all_answers(api_client: TestClient, questions_count: int = 3):
-    for _ in range(questions_count):
-        create_question()
+        response = api_client.delete(f"{self.path}{model_id}")
+        assert response.status_code == status.HTTP_204_NO_CONTENT, response.text
 
-    response = api_client.get(QUESTIONS_PREFIX)
-    assert response.status_code == status.HTTP_200_OK, response.text
-    response_data = response.json()
+        response = api_client.get(f"{self.path}{model_id}")
+        assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
 
-    assert isinstance(response_data, list), response_data
-    assert len(response_data) == questions_count, response_data
-    for question_data in response_data:
-        assert_valid_response(question_data)
-
-
-def test_update_question_without_answers(api_client: TestClient):
-    question = create_question()
-    new_text = "new text"
-
-    response = api_client.patch(
-        f"{QUESTIONS_PREFIX}{question.id}",
-        json={"text": new_text},
-    )
-    assert response.status_code == status.HTTP_200_OK, response.text
-    response_data = response.json()
-    assert_valid_response(response_data)
-
-    assert response_data["id"] >= question.id, response_data
-
-    assert response_data["text"] != question.text
-    assert response_data["text"] == new_text
-
-    assert response_data["excluded"] == question.excluded, response_data
-
-    # TODO: after auth
-    # assert response_data["author"]["id"] != question.author.id
-    # assert response_data["author"]["id"] == new_author.id
-
-    assert response_data["game_type"]["id"] == question.game_type.id, response_data
-    assert response_data["game_type"]["name"] == question.game_type.name
-
-    assert (
-            response_data["game_category"]["id"] == question.game_category.id
-    ), response_data
-    assert response_data["game_category"]["name"] == question.game_category.name
-
-    assert not response_data["answers"], response_data
-
-
-def test_update_question_with_answers(api_client: TestClient):
-    pass
-
-# @pytest.mark.asyncio()
-# async def test_delete_question_without_answers(
-#     api_client: TestClient,
-#     db_session: Session,
-# ):
-#     question = create_question()
-#
-#     response = api_client.delete(f"{QUESTIONS_PREFIX}{question.id}")
-#     assert response.status_code == status.HTTP_204_NO_CONTENT, response.text
-#
-#     response = api_client.get(f"{QUESTIONS_PREFIX}{question.id}")
-#     assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
-#
-#     db_question = await db.get(question.id, Question, db_session)
-#     assert db_question.deleted
-#     assert not db_question.answers
-#
-#
-# @pytest.mark.asyncio()
-# async def test_delete_question_with_answers(
-#     api_client: TestClient,
-#     db_session: Session,
-# ):
-#     question = create_question()
-#     AnswerFactory.create(question_id=question.id)
-#
-#     response = api_client.delete(f"{QUESTIONS_PREFIX}{question.id}")
-#     assert response.status_code == status.HTTP_204_NO_CONTENT, response.text
-#
-#     response = api_client.get(f"{QUESTIONS_PREFIX}{question.id}")
-#     assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
-#
-#     db_question = await db.get(question.id, Question, db_session)
-#     assert db_question.deleted
-#     assert db_question.answers[0].deleted
+        await self.assert_valid_delete(model_id, db_session)
