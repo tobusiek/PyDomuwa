6e70c2a3f1ff1589804c269d7bb2b4d7d63be1b1
tobasqo <szymon.j.jaskowski@gmail.com>
2024-08-04 13:56:28 +0200

    add player router to app

--- backend/domuwa/main.py
+++ backend/domuwa/main.py
@@ -10,6 +10,7 @@ from domuwa.config import settings
 from domuwa.database import create_db_and_tables
 from domuwa.routers.answers_router import get_answers_router
 from domuwa.routers.game_types_router import get_game_types_router
+from domuwa.routers.players_router import get_players_router
 from domuwa.routers.questions_router import get_questions_router
 
 
@@ -23,7 +24,7 @@ async def lifespan(_: FastAPI):
 app = FastAPI(debug=True, lifespan=lifespan)
 
 API_PREFIX = "/api"
-# app.include_router(players_router, prefix=API_PREFIX)
+app.include_router(get_players_router(), prefix=API_PREFIX)
 app.include_router(get_game_types_router(), prefix=API_PREFIX)
 # app.include_router(qna_categories_router, prefix=API_PREFIX)
 app.include_router(get_answers_router(), prefix=API_PREFIX)
