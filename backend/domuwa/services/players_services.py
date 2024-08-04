3819c4aca6329af188cbe0a18a2adbf28176ba42
tobasqo <szymon.j.jaskowski@gmail.com>
2024-08-04 13:55:11 +0200

    refactor player services using common service

--- backend/domuwa/services/players_services.py
+++ backend/domuwa/services/players_services.py
@@ -1,59 +1,18 @@
 import logging
 
-from fastapi import Depends, HTTPException, status
 from sqlmodel import Session, select
 
-from domuwa import database as db
-from domuwa.models.player import Player
-
-logger = logging.getLogger(__name__)
+from domuwa.models.player import Player, PlayerCreate, PlayerUpdate
+from domuwa.services.common_services import CommonServices
 
 
-async def create_player(player: Player, db_sess: Session = Depends(db.get_db_session)):
-    return await db.save(player, db_sess)
+class PlayerServices(CommonServices[PlayerCreate, PlayerUpdate, Player]):
+    def __init__(self) -> None:
+        super().__init__(Player, logging.getLogger(__name__))
 
-
-async def login_player(player: Player, db_sess: Session = Depends(db.get_db_session)):
-    db_player = db_sess.exec(
-        select(Player).where(Player.name == player.name)
-    ).one_or_none()
-    if db_player is None:
-        db_player = await create_player(player, db_sess)
-    return db_player
-
-
-async def get_player_by_id(
-    player_id: int,
-    db_sess: Session = Depends(db.get_db_session),
-):
-    return await db.get(player_id, Player, db_sess)
-
-
-async def get_player_by_name(
-    player_name: str,
-    db_sess: Session = Depends(db.get_db_session),
-):
-    db_player = db_sess.exec(
-        select(Player).where(Player.name == player_name)
-    ).one_or_none()
-    if db_player is None:
-        err_msg = f"Player with name={player_name} not found"
-        logger.error(err_msg)
-        raise HTTPException(status.HTTP_404_NOT_FOUND, err_msg)
-    return db_player
-
-
-async def get_all_players(db_sess: Session = Depends(db.get_db_session)):
-    return await db.get_all(Player, db_sess)
-
-
-async def update_player(
-    player_id: int,
-    player: Player,
-    db_sess: Session = Depends(db.get_db_session),
-):
-    return await db.update(player_id, player, db_sess)
-
-
-async def delete_player(player_id: int, db_sess: Session = Depends(db.get_db_session)):
-    await db.delete(player_id, Player, db_sess)
+    async def create(self, model: PlayerCreate, session: Session):
+        db_player = session.exec(select(Player).where(Player.name == model.name)).first()
+        if db_player is not None:
+            self.logger.warning("Player(name=%s) already exists.", model.name)
+            return None
+        return await super().create(model, session)
