0999c97412985fc1142da120222de4d0b5ff03bd
tobasqo <szymon.j.jaskowski@gmail.com>
2024-08-04 02:20:27 +0200

    fix answer router - 404 on deleted-marked

--- backend/domuwa/routers/answers_router.py
+++ backend/domuwa/routers/answers_router.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
 from typing_extensions import override
 
@@ -18,6 +18,34 @@ class AnswerRouter(CommonRouter[AnswerCreate, AnswerUpdate, Answer]):
     services = AnswerServices()
     logger = logging.getLogger(__name__)
     db_model_type_name = Answer.__name__
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.router.routes.remove(
+            next(route for route in self.router.routes if route.name == "create")  # type: ignore
+        )
+        self.router.add_api_route(
+            "/",
+            self.create,
+            status_code=status.HTTP_201_CREATED,
+            methods=["POST"],
+            response_model=AnswerRead,
+        )
+
+    @override
+    async def get_by_id(
+        self,
+        model_id: int,
+        session: Session = Depends(get_db_session),
+    ):
+        model = await super().get_by_id(model_id, session)
+        if not model.deleted:
+            return model
+
+        err_msg = f"Got Answer(id={model_id}) to get, but it was deleted"
+        self.logger.warning(err_msg)
+        raise HTTPException(status.HTTP_404_NOT_FOUND, err_msg)
 
     @override
     async def create(
